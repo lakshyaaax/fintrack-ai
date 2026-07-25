eb1c6758e97573f810a12b4e2933873f3a0f3dee
Lakshya Khairiya <lakshyakhairiya1@gmail.com>
2026-07-25 12:52:27 +0530

    Implement Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,104 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { register } from "../services/authService";
+
 function Register() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match");
+            return;
+        }
+
+        try {
+
+            await register({
+                name,
+                email,
+                password,
+            });
+
+            alert("Registration Successful! Please login.");
+
+            navigate("/");
+
+        } catch (error) {
+
+            alert(
+                error.response?.data?.message || "Registration Failed"
+            );
+
+        }
+    };
+
     return (
-        <h1>Register Page</h1>
+        <div className="login-container">
+
+            <form
+                className="login-form"
+                onSubmit={handleSubmit}
+            >
+
+                <h1>FinTrack AI 💰</h1>
+
+                <h3>Create Account 🚀</h3>
+
+                <input
+                    type="text"
+                    placeholder="Full Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">
+                    Register
+                </button>
+
+                <p>
+                    Already have an account?{" "}
+                    <Link to="/">
+                        Login
+                    </Link>
+                </p>
+
+            </form>
+
+        </div>
     );
 }
 

0f3d8effcf71cbafcc617045cec967a28c75f826
Lakshya Khairiya <lakshyakhairiya1@gmail.com>
2026-08-10 14:26:28 +0530

    updated backend URL + fixes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://your-pythonanywhere-url/api", // backend URL
+  baseURL: "https://lakshya123.pythonanywhere.com/api", // backend URL
 });
 
 // Attach token automatically
